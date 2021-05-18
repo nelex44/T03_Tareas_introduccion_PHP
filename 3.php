5023a621c87d10089738a63cefc2d8c5df46b07c
Daniel Valiente <74688265+nelex44@users.noreply.github.com>
2021-05-18 12:02:21 +0200

    Actualización de datos
    Se suben los 3 ejercicios de php y varios

---
+++ 3.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Introducción a los arrays, metodo corto</title>
+    </head>
+    <body>
+        <h1>Introducción a los arrays, metodo corto</h1>
+        <p>A continuación escribiremos arrays de acuerdo al metodo corto</p>
+        <p>
+            <?php
+            /*
+            Este es el método corto para la creación de arrays donde cada todo el array puede estar
+            en una sola linea de código
+            */
+            $dia = array("domingo","lunes","martes","miercoles","jueves","viernes","sabado");
+            //mostrar el miercoles
+            echo $dia[3];
+            ?>
+        </p>
+    </body>
+</html>
